f47550e77458d0fba34c9390fd93f870b2dceaab
Pauline <pauline.olivier06@yahoo.fr>
2023-03-29 15:51:03 +0200

    update axis deformation

--- cgogn/modeling/types/graph_parameters.h
+++ cgogn/modeling/types/graph_parameters.h
@@ -33,6 +33,7 @@
 #include <cgogn/geometry/algos/selection.h>
 
 #include <cgogn/ui/view.h>
+#include <algorithm>
 
 namespace cgogn
 {
@@ -78,10 +79,9 @@ public:
 	GraphParameters(GraphParameters&&) = default;
 	GraphParameters& operator=(GraphParameters&&) = default;
 
-	/**
-	 * update selected vertices vbo 
-	 * selected vertices turns into red
-	*/
+
+	/// @brief update selected vertices 
+	/// update vbo 
 	void update_selected_vertices_vbo()
 	{
 		if (selected_vertices_set_)
@@ -103,7 +103,15 @@ public:
 	 * select vertices set 
 	 * use picking sphere to raycast on a sphere
 	*/
-	void select_vertices_set(ui::View* view, const int32& button, const int32& x, const int32& y)
+
+	/// @brief select vertices set 
+	/// call picking sphere to raycast on a sphere 
+	/// @param view current view
+	/// @param button select (left) or unselect (right)
+	/// @param x retrieved from mouse position
+	/// @param y retrieved from mouse position
+	void select_vertices_set(ui::View* view, const int32& button, 
+							const int32& x, const int32& y)
 	{
 		if (selected_vertices_set_)
 		{
@@ -186,101 +194,11 @@ public:
 		{
 			if (selected_vertices_set_->size() == 1)
 			{
-				selected_vertices_set_->foreach_cell([&](Vertex v) {
-					if (v.index_ == 0 || v.index_ == number_of_handles_-1)
-					{
-						std::vector<Edge>& edges = 
-								(*graph_->vertex_incident_edges_)[v.index_];
-
-						Edge e0 = edges[0];
-						if ((*graph_->edge_incident_vertices_)[e0.index_]
-																.first == v)
-						{
-							Vertex v1 = 
-								(*graph_->edge_incident_vertices_)[e0.index_]
-												.second;
-							rotation_center_ = 
-								value<Vec3>(*graph_, vertex_position_, v1);
-						}
-						else
-						{
-							Vertex v1 = 
-								(*graph_->edge_incident_vertices_)[e0.index_]
-									.first;
-							rotation_center_ = 
-								value<Vec3>(*graph_, vertex_position_, v1);
-						}
-					}
-				});
+				select_one_vertex(); 
 			}
-			else if (selected_vertices_set_->size() == 2)
+			else
 			{
-				std::vector<Vertex> valid_vertex;
-				selected_vertices_set_->foreach_cell([&](Vertex v) {
-					if (v.index_ == 0 || v.index_ == number_of_handles_ -1)
-					{
-						std::vector<Edge>& edges = 
-							(*graph_->vertex_incident_edges_)[v.index_];
-						Edge e0 = edges[0];
-						Vertex v1;
-						Vertex target_vertex; 
-						if ((*graph_->edge_incident_vertices_)[e0.index_]
-															.first == v)
-						{
-							v1 = (*graph_->edge_incident_vertices_)
-													[e0.index_].second;
-						}
-						else
-						{
-							v1 = (*graph_->edge_incident_vertices_)
-														[e0.index_].first;
-						}
-
-						std::vector<Edge>& edges1 = 
-							(*graph_->vertex_incident_edges_)[v1.index_];
-
-						Edge e1_0 = edges1[0];
-						if ((*graph_->edge_incident_vertices_)
-							[e1_0.index_].first == v ||
-							(*graph_->edge_incident_vertices_)
-							[e1_0.index_].second == v)
-						{
-							Edge e1_1 = edges1[1];
-							if ((*graph_->edge_incident_vertices_)
-													[e1_1.index_].first == v1)
-							{
-								target_vertex = 
-									(*graph_->edge_incident_vertices_)
-													[e1_1.index_].second;
-							}
-							else
-							{
-								target_vertex = 
-									(*graph_->edge_incident_vertices_)
-													[e1_1.index_].first;
-							}
-						}
-						else
-						{
-							if ((*graph_->edge_incident_vertices_)
-											[e1_0.index_].first == v1)
-							{
-								target_vertex = 
-									(*graph_->edge_incident_vertices_)
-												[e1_0.index_].second;
-							}
-							else
-							{
-								target_vertex = 
-									(*graph_->edge_incident_vertices_)
-										[e1_0.index_].first;
-							}
-						}
-						rotation_center_ = 
-						value<Vec3>(*graph_, vertex_position_, target_vertex);
-					}
-						
-				});
+				select_multiple_vertices(); 
 			}
 
 			dragging_axis_ = true;
@@ -310,6 +228,8 @@ public:
 			for (size_t t = 0; t < transformations_.size(); t++){
 				transformations_[t].setIdentity();
 			}
+
+			transformations_valid_indices_.clear(); 
 			
 		}
 			
@@ -387,6 +307,7 @@ public:
 	Vec3 rotation_center_;
 
 	std::vector<rendering::Transfo3d> transformations_;
+	std::vector<std::size_t> transformations_valid_indices_; 
 
 	
 	float64 drag_z_;
@@ -395,6 +316,152 @@ public:
 private: 
 	bool dragging_handle_;
 	bool dragging_axis_;
+
+	/// @brief select one vertex
+	/// Two cases: extremity vertex or not 
+	/// TODO: non extremity case
+	void select_one_vertex()
+	{
+
+		Vertex selected_vertex;  
+		selected_vertices_set_->foreach_cell([&](Vertex v) {
+			selected_vertex = v; 
+		});
+
+		std::vector<Edge>& edges = 
+					(*graph_->vertex_incident_edges_)[selected_vertex.index_];
+
+		Edge e0 = edges[0]; 
+		std::pair<Vertex, Vertex> first_edge = 
+								(*graph_->edge_incident_vertices_)[e0.index_]; 
+
+		if (first_edge.first == selected_vertex){
+			rotation_center_ = value<Vec3>(*graph_, vertex_position_, 
+															first_edge.second);
+		} else {
+			rotation_center_ = value<Vec3>(*graph_, vertex_position_, 
+															first_edge.first);
+		} 
+
+		std::cout << selected_vertex.index_ << std::endl; 
+		if (selected_vertex.index_ == 0){
+			std::cout << "case 0 index selected" << std::endl; 
+			transformations_valid_indices_.push_back(1); 
+		} else {
+			if (selected_vertex.index_ == number_of_handles_ -1){
+				std::cout << "case last index " << std::endl; 
+				transformations_valid_indices_.push_back(selected_vertex.index_); 
+			} else {
+				std::cout << "TODO" << std::endl; 
+			}
+		}
+	}
+	
+
+	/// @brief select multiple vertices
+	/// Two cases: successive vertices or not 
+	/// TODO: not successive vertices 
+	void select_multiple_vertices()
+	{
+
+		std::vector<Vertex> selected_vertices; 
+		selected_vertices_set_->foreach_cell([&](Vertex v) {
+			selected_vertices.push_back(v); 
+		});
+
+		std::sort(selected_vertices.begin(), selected_vertices.end(), 
+				[](Vertex a, Vertex b){return a.index_ < b.index_;});
+
+		if (selected_vertices[0].index_ == 0){
+			Vertex last_vertex = 
+								selected_vertices[selected_vertices.size()-1]; 
+
+			std::vector<Edge>& edges = 
+						(*graph_->vertex_incident_edges_)[last_vertex.index_];
+			
+			Edge e0 = edges[0];
+			Vertex previous_vertex = 
+								selected_vertices[selected_vertices.size()-2];
+
+			std::pair<Vertex, Vertex> first_edge = 
+								(*graph_->edge_incident_vertices_)[e0.index_]; 
+			
+			if (first_edge.first == previous_vertex || 
+				first_edge.second == previous_vertex)
+			{
+				Edge e1 = edges[1]; 
+				std::pair<Vertex, Vertex> second_edge = 
+								(*graph_->edge_incident_vertices_)[e1.index_]; 
+				
+				if (second_edge.first == last_vertex){
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, second_edge.second);
+				} else {
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, second_edge.first);
+				}
+				
+			} else {
+				if (first_edge.first == last_vertex){
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, first_edge.second);
+				} else {
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, first_edge.first);
+				}
+			}
+
+			for (std::size_t i = 0; i < selected_vertices.size() -1; i++){
+				transformations_valid_indices_.push_back(i+1); 
+			}
+		} 
+		else if (selected_vertices[selected_vertices.size()-1].index_ == 
+														number_of_handles_ -1)
+		{
+			Vertex first_vertex = selected_vertices[0]; 
+
+			std::vector<Edge>& edges = 
+						(*graph_->vertex_incident_edges_)[first_vertex.index_];
+			
+			Edge e0 = edges[0];
+			Vertex next_vertex = selected_vertices[1];
+
+			std::pair<Vertex, Vertex> first_edge = 
+								(*graph_->edge_incident_vertices_)[e0.index_]; 
+			
+			if (first_edge.first == next_vertex || 
+				first_edge.second == next_vertex)
+			{
+				Edge e1 = edges[1]; 
+				std::pair<Vertex, Vertex> second_edge = 
+								(*graph_->edge_incident_vertices_)[e1.index_]; 
+				
+				if (second_edge.first == first_vertex){
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, second_edge.second);
+				} else {
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, second_edge.first);
+				}
+				
+			} else {
+				if (first_edge.first == first_vertex){
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, first_edge.second);
+				} else {
+					rotation_center_ = 
+						value<Vec3>(*graph_, vertex_position_, first_edge.first);
+				}
+			}
+
+			for (std::size_t i = 0; i < selected_vertices.size() -1 ; i++){
+				transformations_valid_indices_.push_back(number_of_handles_ - (i+1)); 
+			}
+		} else {
+			std::cout << "TODO" << std::endl; 
+		}
+		
+	}
 
 	/// @brief handle displacement caused by the mouse displacement
 	/// handle translates along normal 
@@ -449,35 +516,17 @@ private:
 					Eigen::Translation3d(rotation_center_) * rot 
 						* Eigen::Translation3d(-rotation_center_);
 
-				if (selected_vertices_set_->size() == 1){
-					
-					selected_vertices_set_->foreach_cell([&](Vertex v) {
-					Vec3& axis_vertex_position = 
-						value<Vec3>(*graph_, vertex_position_, v);
-
-					axis_vertex_position = M * axis_vertex_position;
-					if (v.index_ == 0)
-					{
-						transformations_[1] = M;
-					}
-					else if (v.index_ == number_of_handles_ -1)
-					{
-						transformations_[v.index_] = M;
-					}
-					
-				});
-				} else if (selected_vertices_set_->size() == 2){
-					selected_vertices_set_->foreach_cell([&](Vertex v) {
-						Vec3& axis_vertex_position = 
-							value<Vec3>(*graph_, vertex_position_, v);
+				selected_vertices_set_->foreach_cell([&](Vertex v) {
+					Vec3& axis_vertex_position = value<Vec3>(*graph_, 
+															vertex_position_, v);
 						axis_vertex_position = M * axis_vertex_position;
 					}); 
 
-					transformations_[0] = M;
-					transformations_[1] = M;
+				for (size_t t = 0; t < transformations_valid_indices_.size(); t++){ 
+					transformations_[transformations_valid_indices_[t]] = M;
 				}
-				
 			}
+				
 	}
 
 
